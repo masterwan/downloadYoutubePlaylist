5cd796fb9ae886f8f20967650aac4bc033cf2d9e
masterwan <vbegunof@yandex.ru>
2019-04-20 14:16:18 +0300

    add exception 'this video is not available'

--- getlink.py
+++ getlink.py
@@ -7,9 +7,12 @@ for link in links:
 	link = "https://youtube.com/watch?" + link.split('&amp;')[0]
 	print(link + ' | video: --> ' + str(i))
 	i += 1
-	yt = YouTube(link)
-	#yt = yt.get('mp4', '720p')
-	yt = yt.streams.filter(progressive=True, file_extension='mp4').first()
-	if not os.path.exists('./download'): os.makedirs('./download')
-	yt.download('./download')
+	try:
+		yt = YouTube(link)
+		#yt = yt.get('mp4', '720p')
+		yt = yt.streams.filter(progressive=True, file_extension='mp4').first()
+		if not os.path.exists('./download'): os.makedirs('./download')
+		yt.download('./download')
+	except:
+		print('this video is not available')	
 print('-----------------------------end-------------------------------')

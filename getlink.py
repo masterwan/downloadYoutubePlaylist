4bde6762d16d33eeedc86bf6c9e2c4bff4720fa4
masterwan <vbegunof@yandex.ru>
2019-04-20 15:54:39 +0300

    the addition of three attempts to download

--- getlink.py
+++ getlink.py
@@ -2,17 +2,32 @@ from pprint import pprint as print
 from pytube import YouTube
 import requests,re,os
 links = set(re.findall(r'href="/watch\?(.*?)"', requests.get(input('playlist youtube link:')).text));
+
+def dlYoutube(link, path):
+	yt = YouTube(link)
+	#yt = yt.get('mp4', '720p')
+	yt = yt.streams.filter(progressive=True, file_extension='mp4').first()
+	if not os.path.exists(path): os.makedirs(path)
+	yt.download(path)
+	print('--*< - success - >*--')	
+
 i = 1
+path = './download'
 for link in links:
 	link = "https://youtube.com/watch?" + link.split('&amp;')[0]
 	print(link + ' | video: --> ' + str(i))
 	i += 1
 	try:
-		yt = YouTube(link)
-		#yt = yt.get('mp4', '720p')
-		yt = yt.streams.filter(progressive=True, file_extension='mp4').first()
-		if not os.path.exists('./download'): os.makedirs('./download')
-		yt.download('./download')
+		dlYoutube(link, path)
 	except:
-		print('this video is not available')	
+		try:
+			print('attempt 2')
+			dlYoutube(link, path)
+		except:
+			try:
+				print('attempt 3')
+				dlYoutube(link, path)	
+			except:
+				print('this video is not available')
+
 print('-----------------------------end-------------------------------')
